0db0d4e556383e2bc8c2f3959c542e57060cac4d
santosh sanap <santoshsanap143145@gmail.com>
2025-02-27 02:05:36 +0530

    list demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'outFirstApp';
+  title = 'Angular Task - 5 Set of Lists';
+
+
+drinks : Array<string> = ["Orange juice", "Iced coffee", "Green tea", "Chocolate milk", "Lemon soda"];
+
+houseItems : Array<string> = ["Remote control", "Table lamp", "Wall clock", "Door mat", "Ceiling fan"];
+
+
+outdoorActivities : Array<string> = ["Riding a bicycle", "Flying a kite", "Hiking a trail", "Playing soccer", "Camping in the woods"];
+
+
+pencilCaseItems : Array<string> = ["Mechanical pencil", "Ballpoint pen", "Eraser block", "Highlighter pen", "Sticky notes"];
+
+
+randomObjects : Array<string> = ["Bluetooth speaker", "Sunglasses case", "Wristwatch strap", "Phone charger", "Car key fob"];
 }
+
+
